1ee8b5471a7e2982525035418c0ae50a5b7b79bf
ilix-studio <ilishjyoti17@gmail.com>
2025-05-29 09:17:36 +0530

    Fix CORS error test/5

--- src/config/allowOrigins.ts
+++ src/config/allowOrigins.ts
@@ -1,7 +1,7 @@
 const allowOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://prapti-foundation-site.vercel.app/",
+  "https://prapti-foundation-site.vercel.app",
   "https://prapti-foundation-site.vercel.app/admin/login",
   "https://prapti-foundation-site.vercel.app/admin/dashboard",
   "https://prapti-foundation-site.vercel.app/blog",
