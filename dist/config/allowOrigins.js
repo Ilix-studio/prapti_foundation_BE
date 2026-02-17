9784da3003c63ed37dc762f8b1e8f2a03c9cb10a
ilix-studio <ilishjyoti17@gmail.com>
2026-02-17 15:52:50 +0530

    build/3

--- dist/config/allowOrigins.js
+++ dist/config/allowOrigins.js
@@ -3,14 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const allowOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
+    "https://praptifoundation.in",
     "https://www.praptifoundation.in",
-    "https://www.praptifoundation.in/",
-    "https://www.praptifoundation.in/admin/login",
-    "https://www.praptifoundation.in/admin/dashboard",
-    "https://www.praptifoundation.in/blog",
     "https://biswajitphukan.com",
     "https://www.biswajitphukan.com",
-    "",
 ];
 // Add environment-specific origins
 if (process.env.NODE_ENV === "production" && process.env.FRONTEND_URL) {
--- src/config/allowOrigins.ts
+++ src/config/allowOrigins.ts
@@ -1,14 +1,10 @@
 const allowOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
+  "https://praptifoundation.in",
   "https://www.praptifoundation.in",
-  "https://www.praptifoundation.in/",
-  "https://www.praptifoundation.in/admin/login",
-  "https://www.praptifoundation.in/admin/dashboard",
-  "https://www.praptifoundation.in/blog",
   "https://biswajitphukan.com",
   "https://www.biswajitphukan.com",
-  "",
 ];
 
 // Add environment-specific origins
